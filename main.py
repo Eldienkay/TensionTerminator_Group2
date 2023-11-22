36c2c7adb7017cc2b2dfa06a95294a9ab142c188
Eldienkay <Eldienkay@users.noreply.github.com>
2023-11-22 20:14:22 +0100

    Distance
    Verschlimmbessert leider... Bin aber an einer Lösung dran

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import cv2
-import mediapipe as mp
-from collections import deque
-import numpy as np
+from HelperFunctions import *
 
 
 class MovingAverageFilter:
@@ -21,11 +18,11 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
 # Create deques to store the last 10 positions for each shoulder
-left_shoulder_buffer = deque(maxlen=8)
-right_shoulder_buffer = deque(maxlen=8)
+left_shoulder_buffer = deque(maxlen=20)
+right_shoulder_buffer = deque(maxlen=20)
 
 
-def process_frame(frame):
+def process_frame(frame, calibration_data):
     # Convert the BGR image to RGB.
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # Process the image and find pose landmarks.
@@ -35,6 +32,8 @@ def process_frame(frame):
     image_with_pose = frame.copy()
 
     if result.pose_landmarks:
+
+        # Extract landmarks
         left_shoulder = result.pose_landmarks.landmark[11]
         right_shoulder = result.pose_landmarks.landmark[12]
 
@@ -60,11 +59,59 @@ def process_frame(frame):
         cv2.line(image_with_pose, (avg_left_shoulder[0], avg_left_shoulder[1]),
                  (avg_right_shoulder[0], avg_right_shoulder[1]), (0, 0, 255), 2)
 
-    return image_with_pose
+        # Calculate the size of the head in pixels
+        head_size_pixels = calculate_head_size_in_pixels(result.pose_landmarks.landmark, frame)
+
+        # Estimate distance using the calibration data
+        distance = estimate_distance(head_size_pixels, calibration_data)
+
+        if distance >= 2:
+            distance = 2
+
+        # Display the distance information
+        cv2.putText(frame, f"Distance: {distance:.2f} meters", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    return frame
+
+
+def calculate_head_size_in_pixels(landmarks, frame):
+    # Example using NOSE and SHOULDER landmarks
+    nose = landmarks[mp_pose.PoseLandmark.NOSE]
+    left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+    right_shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+
+    # Estimate the top of the head as a point above the NOSE
+    estimated_top_of_head_x = nose.x
+    estimated_top_of_head_y = nose.y - (abs(left_shoulder.y - right_shoulder.y) * 0.5)
+
+    # Convert these landmarks to pixel coordinates
+    nose_x = int(nose.x * frame.shape[1])
+    nose_y = int(nose.y * frame.shape[0])
+    top_of_head_x = int(estimated_top_of_head_x * frame.shape[1])
+    top_of_head_y = int(estimated_top_of_head_y * frame.shape[0])
+
+    # Calculate the size (e.g., height) of the head in pixels
+    head_size_pixels = np.sqrt((nose_x - top_of_head_x)**2 + (nose_y - top_of_head_y)**2)
+
+    return head_size_pixels
+
+
+def estimate_distance(ref_size_pixels, calibration_data):
+    known_distance, known_size_pixels = calibration_data
+
+    # To prevent division by zero
+    if ref_size_pixels == 0:
+        return float('inf')
+
+    distance = (known_size_pixels / ref_size_pixels) * known_distance
+    return distance
 
 
 def main():
-    cap = cv2.VideoCapture('videos/RGB_video1.mp4')
+    cap = cv2.VideoCapture('videos/duo_balls_1.mp4')
+
+    calibration_data = (1.0, 30)
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -72,7 +119,7 @@ def main():
             break
 
         # Process frame with MediaPipe to get pose landmarks
-        frame_with_pose = process_frame(frame)
+        frame_with_pose = process_frame(frame, calibration_data)
 
         # Display the frame with pose landmarks
         cv2.imshow('Frame', frame_with_pose)
